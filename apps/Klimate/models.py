6a40f8b495c20128af1217c7c0182ec9477b0033
Robin Hartley <robinhartley@Robins-MacBook-Pro.local>
2019-04-10 21:07:09 -0700

    updated css

--- apps/Klimate/models.py
+++ apps/Klimate/models.py
@@ -1,8 +1,11 @@
+from __future__ import unicode_literals
 from django.db import models
 
 class Marijuana(models.Model):
     Name = models.TextField()
     Type = models.TextField()
-    ParentStrain = models.TextFiel()
+    ParentStrain1 = models.TextField()
+    ParentStrain2 = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
--- apps/Klimate/views.py
+++ apps/Klimate/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render, redirect
-
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
-def one_method(request):                # no values passed via URL
-    pass  
+    return render(request, "Klimate/index.html")
